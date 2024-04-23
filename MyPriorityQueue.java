97e5b61fe680d71a3d5058a0c9b2c4b051449226
Kelig Ngabo <144262857+kngabo@users.noreply.github.com>
2024-04-23 03:23:17 +0000

    New

--- MyPriorityQueue.java
+++ MyPriorityQueue.java
@@ -26,4 +26,40 @@ public class MyPriorityQueue<T> extends AbstractQueue<T>{
         this.heap.clear();
         this.heap.add(null);
     }
+
+    public int parent(int index){
+        return index/2;
+    }
+
+    public int leftChild(int index){
+        return 2 * index;
+    }
+
+    public int Child(int index){
+        return 2 * index + 1;
+    }
+
+    public boolean offer(T item){
+        int size = this.size();
+        this.heap.add(item);
+        percolateUp(this.heap.size());
+        
+        return this.size() == size + 1 ;
+    }
+
+    private void percolateUp(int index){
+
+        int parent = parent(index);
+
+        if(parent(index) == 0){
+            return;
+        }
+
+        if(comparator.compare(this.heap.get(index), this.heap.get(parent)) < 0){
+            this.heap.set(parent, this.heap.get(index));
+            this.heap.set(index, this.heap.get(parent));
+
+            percolateUp(parent);
+        }
+    }
 }
