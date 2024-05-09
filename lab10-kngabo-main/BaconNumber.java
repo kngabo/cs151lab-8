8bcef6a7d41038c750640987e31427435d5576ca
Kelig Ngabo <144262857+kngabo@users.noreply.github.com>
2024-05-09 00:57:47 +0000

    New

--- lab10-kngabo-main/BaconNumber.java
+++ lab10-kngabo-main/BaconNumber.java
@@ -1,14 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class BaconNumber {
     
     private Graph graph;
     
-    private void helper(){
+    public BaconNumber(String filePath) throws FileNotFoundException{
+        this.graph = graph;
+        
+        this.helper(filePath);
+    }
+    private void helper(String filePath){
 
-        Set<String> name = new HashSet<String>();
-        Set<String> title = new HashSet<String>();
-        Map<String, List<String>> map = new HashMap<>();
-        map.put(name., Arrays.asList(title));
+        Set<String> names = new HashSet<String>();
+        Set<String> titles = new HashSet<String>();
+        Map<String, List<String>> actorsToMovies = new HashMap<>();
+        Map<List<String>, String> moviesToActors = new HashMap<>();
+        List<String> tList = new ArrayList<String>();
+
+        File file = new File(filePath);
+
+        Scanner lineScanner = new Scanner(file);
+
+        while(lineScanner.hasNextLine()){
+
+            String line = lineScanner.nextLine();
+            String[] split = line.split("\t");
+            String name = split[0];
+            String title = split[1];
+
+            names.add(name);
+            titles.add(title);
+            tList.add(title);
+            actorsToMovies.put(name, tList);
+            moviesToActors.put(tList, name);
+
+        }
+
+        for (String actor : names) {
+            Vertex vertex = new Vertex(actor);
+            graph.addVertex(vertex);
+        }
+
+        for (Vertex vertex : graph.getVertexList()) {
+            
+        }
+
+
+        }
+
     }
 }
